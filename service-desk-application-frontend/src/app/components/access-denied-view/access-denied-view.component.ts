2c977c75ef6b373e938d3ceecea2a4f91510694d
Bartosz Cieślak <cbartosz.99@gmail.com>
2024-05-05 16:26:34 +0200

    [FE] Dodano widok z komunikatem o braku uprawnień

--- service-desk-application-frontend/src/app/components/access-denied-view/access-denied-view.component.ts
+++ service-desk-application-frontend/src/app/components/access-denied-view/access-denied-view.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-access-denied-view',
   templateUrl: './access-denied-view.component.html',
   styleUrls: ['./access-denied-view.component.css']
 })
-export class AccessDeniedViewComponent {}
+export class AccessDeniedViewComponent
+{
+  constructor(private authService: AuthService) {}
+
+  logout(): void
+  {
+    this.authService.logoutUser();
+  }
+}
